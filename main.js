04203664688e14509997e3d1e055bb60cef66baa
WarClouser <ss4039417@gmail.com>
2023-05-14 03:25:45 +0530

    some more

--- main.js
+++ main.js
@@ -53,3 +53,29 @@ gsap.from(".bottle-even", {
     start: "top center",
   },
 });
+
+$('.your-class').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
